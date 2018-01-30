3464171e0125b5e45fe30c92757d0cea08d7591c
thanhthao993 <nicole.thao93@gmail.com>
2018-01-30 20:15:47 +0700

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -39,8 +39,8 @@ export class HomeComponent implements OnInit {
   constructor(private _data: DataService) { }
 
   ngOnInit() {
-    this.itemCount = this.goals.length;
     this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
 
